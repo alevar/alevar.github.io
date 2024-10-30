21b911f7c45fb8ae4be66540eb2adf073eb2ebe6
Alex Sparrow <alevar@gmail.com>
2024-10-30 16:21:29 -0400

    react init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home: React.FC = () => {
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Home</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
