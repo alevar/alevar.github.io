56a6aa643c63b127698218d2514f54c74086b77f
Alex Sparrow <alevar@gmail.com>
2024-11-02 04:46:43 -0400

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    sourcemap: true, // Enable source maps for production builds if needed
-  }
+  base: '/alevar.github.io/'
 })
